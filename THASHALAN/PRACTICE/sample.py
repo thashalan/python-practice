d89a31b3ba8015ffd645a78075c2fb5242df3708
Thashalan <thashalanbasakar@gmail.com>
2026-05-23 23:01:04 +0530

    day 1 practice

---
+++ THASHALAN/PRACTICE/sample.py
@@ -0,0 +1,36 @@
+x = 19
+y = "thashalan"
+print(x)
+print(y)
+
+
+d = "DURGA"
+s = "SABI"
+print(d,s)
+
+d = {1:"dhe",2:"culprit"}
+print(d[1])
+print(d.get(2))
+
+a= input("Enter yor name : ")
+b= input("Enter your age : ")
+print(a)
+print(b)
+print("HELLO",a,"WELCOME!!")
+
+
+a =str(input("Enter your name : "))
+b =int(input("Enter your age : "))
+c = input("Enter your address : ")
+d = int(input("Enter your mobile number : "))
+e = int(input("Enter you 12th mark : "))
+f = input("Enter your gmail : ")
+g = input("Enter your collage name : ")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+
